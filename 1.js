22fd99066a6ab31c6523d1420750225a2bdbde91
Aman Gupta <coderamang02@gmail.com>
2023-10-14 22:31:32 +0530

    xypo change 3

--- 1.js
+++ 1.js
@@ -1,6 +1,19 @@
 let menu = document.querySelector(".menu_icon")
 let cont = document.querySelector(".menu_cont")
+let media = document.querySelector(".media")
 menu.addEventListener("click",()=>{
     cont.classList.toggle("show_cont")
     console.log("clicked")
 })
+function LWindow(){
+window.open('https://www.linkedin.com/in/aman-gupta-721953250/');
+}
+function iWindow(){
+    window.open('https://www.instagram.com/');
+}
+function fWindow(){
+    window.open('https://www.facebook.com/profile.php?id=100034360563388');
+}
+function tWindow(){
+    window.open('https://twitter.com/home');
+}
